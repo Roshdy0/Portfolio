607b8e0b9799dfad67ee1a9410dbc0a0d6525ca3
Roshdy Mammdouh <122674292+Roshdy-Mammdouh@users.noreply.github.com>
2023-06-12 08:48:56 +0200

    1

---
+++ My-Projcts/1-App-Land/js/home.js
@@ -0,0 +1,81 @@
+var typed = new Typed('.animate', {
+    strings: [
+      "your apps",
+      "Your Device",
+      "Mobile Apps",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+  });
+
+  /* Change Header With Scroll Bar */
+window.onscroll = function() {headerFun()};
+function headerFun() {
+
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+      document.getElementById("header").style.padding = "10px";
+      document.getElementById("header").classList.add("gradiant-background");
+
+      document.getElementById("logo").style.display = "none";
+      document.getElementById("logo2").style.display = "block";
+    } 
+    else {
+        document.getElementById("header").classList.remove("gradiant-background");
+        document.getElementById("header").style.padding = "30px 10px";
+        
+
+      document.getElementById("logo").style.display = "block";
+      document.getElementById("logo2").style.display = "none";
+
+    }   
+}
+
+/* Change Header With Media Query */
+//Animate Gear And Show Setting
+let gear = document.querySelector(".header .fa-bars");
+let navbar = document.querySelector(".header .navbar-dropdown"); 
+gear.onclick = function() {
+    this.classList.toggle("fa-bars-staggered");
+    navbar.classList.toggle("hidden");
+    navbar.classList.toggle("show");
+    
+};
+
+/* owl Slider Center Mode */
+var owl = $('.first-owl');
+owl.owlCarousel({
+    items:5,
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:10000,
+    dotsEach:true,
+    
+});
+var owl = $('.second-owl');
+owl.owlCarousel({
+    items:2,
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:8000,
+    dotsEach:true,
+    
+});
+
+ // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  document.addEventListener('contextmenu', (e) => e.preventDefault());
+  function ctrlShiftKey(e, keyCode) {
+    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+  }
+  document.onkeydown = (e) => {
+    if (
+      event.keyCode === 123 ||
+      ctrlShiftKey(e, 'I') ||
+      ctrlShiftKey(e, 'J') ||
+      ctrlShiftKey(e, 'C') ||
+      (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    )
+      return false;
+  }; 
---
+++ js/My-Cv.js
@@ -0,0 +1,41 @@
+/* Start NavBar */
+let NavBarOffSetTop = document.querySelector(".navbar");
+var rootHeaderColor = document.querySelector(':root');
+
+window.onscroll = function () {
+    if ( window.scrollY > NavBarOffSetTop.offsetTop + 50 ) {
+        NavBarOffSetTop.style.setProperty ("--header-color", "#ea4020");
+        rootHeaderColor.style.setProperty('--header-text-color', '#fff');
+        rootHeaderColor.style.setProperty('--header-hight', '50px');
+    }else{
+        NavBarOffSetTop.style.setProperty ("--header-color", "#eeeeee5e");
+        rootHeaderColor.style.setProperty('--header-text-color', '#000');
+        rootHeaderColor.style.setProperty('--header-hight', '80px');
+    }
+}
+/* Start About Owl */
+var owl = $('.owl-box');
+owl.owlCarousel({
+    items:1,
+    loop:true,    
+    autoplay:true,
+    dots:false,
+});
+var owl = $('.owl-box2');
+owl.owlCarousel({
+    items:3,
+    loop:true,   
+    margin: 0, 
+    autoplay:true,
+    dots:false,
+});
+var owl = $('.owl-box3');
+owl.owlCarousel({
+    items:5,
+    loop:true,   
+    margin: 0, 
+    autoplay:true,
+    dots:false,
+});
+/* End About Owl */
+/* End NavBar */
