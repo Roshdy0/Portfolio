a9cbc612cbeee58561241e7e501f9ced256a5b1d
Roshdy Mammdouh <roshdy.mammdouh@gmail.com>
2024-01-14 16:19:16 +0200

    0

--- js/My-Cv.js
+++ js/My-Cv.js
@@ -1,16 +1,20 @@
 /* Start NavBar */
 let NavBarOffSetTop = document.querySelector(".navbar");
 var rootHeaderColor = document.querySelector(':root');
+var Up = document.getElementById("Up");
 
 window.onscroll = function () {
     if ( window.scrollY > NavBarOffSetTop.offsetTop + 50 ) {
         NavBarOffSetTop.style.setProperty ("--header-color", "#ea4020");
         rootHeaderColor.style.setProperty('--header-text-color', '#fff');
         rootHeaderColor.style.setProperty('--header-hight', '50px');
+        rootHeaderColor.style.setProperty('--header-hight', '50px');
+        Up.style.display = "block";
     }else{
         NavBarOffSetTop.style.setProperty ("--header-color", "#eeeeee5e");
         rootHeaderColor.style.setProperty('--header-text-color', '#000');
         rootHeaderColor.style.setProperty('--header-hight', '80px');
+        Up.style.display = "none";
     }
 }
 /* Start About Owl */
